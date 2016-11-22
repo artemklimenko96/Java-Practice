7f1f434d8b6aef86d6c24c983ab3ea2ac11624bc
artemklimenko96 <artemklimenko96@gmail.com>
2016-11-23 00:22:09 +0200

    three tasks completed

--- level21/lesson16/big01/Hippodrome.java
+++ level21/lesson16/big01/Hippodrome.java
@@ -1,8 +1,21 @@
 package com.javarush.test.level21.lesson16.big01;
 
+import java.util.ArrayList;
+
 /**
  * Created by Artem Klimenko on 23 Nov 2016.
  */
 public class Hippodrome
 {
+    ArrayList<Horse> horses = new ArrayList<>();
+
+    public ArrayList<Horse> getHorses()
+    {
+        return horses;
+    }
+
+    public static void main(String[] args)
+    {
+
+    }
 }
