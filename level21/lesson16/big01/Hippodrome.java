d6b2efca006868dc751d760699ed0e8bcc48ab05
artemklimenko96 <artemklimenko96@gmail.com>
2016-12-01 19:28:46 +0200

    BigTask 01 solved

--- level21/lesson16/big01/Hippodrome.java
+++ level21/lesson16/big01/Hippodrome.java
@@ -1,5 +1,8 @@
 package com.javarush.test.level21.lesson16.big01;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -7,6 +10,7 @@ import java.util.ArrayList;
  */
 public class Hippodrome
 {
+    public static Hippodrome game;
     ArrayList<Horse> horses = new ArrayList<>();
 
     public ArrayList<Horse> getHorses()
@@ -16,6 +20,50 @@ public class Hippodrome
 
     public static void main(String[] args)
     {
+        game = new Hippodrome();
+        Horse horse1 = new Horse("Matta", 3, 0);
+        Horse horse2 = new Horse("Seryoga", 3,0);
+        Horse horse3 = new Horse("Sobchak", 3,0);
+        game.getHorses().add(horse1);
+        game.getHorses().add(horse2);
+        game.getHorses().add(horse3);
+        game.run();
+    }
 
+    public void run(){
+
+        for (int i = 0; i <100 ; i++)
+        {
+
+            move();
+            print();
+            try
+            {
+                Thread.sleep(500);
+            }
+            catch (InterruptedException e)
+            {
+                System.out.println("triggered");
+            }
+
+        }
+
+
+     }
+
+
+    public void print(){
+        for (Horse h:horses)
+        {
+            h.print();
+            System.out.println();
+            System.out.println();
+        }
+    }
+    public void move(){
+        for (Horse h:horses)
+        {
+            h.move();
+        }
     }
 }
--- level21/lesson16/big01/Horse.java
+++ level21/lesson16/big01/Horse.java
@@ -5,4 +5,56 @@ package com.javarush.test.level21.lesson16.big01;
  */
 public class Horse
 {
+    String name;
+    double speed;
+    double distance;
+
+    public Horse(String name, double speed, double distance)
+    {
+        this.name = name;
+        this.speed = speed;
+        this.distance = distance;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public double getSpeed()
+    {
+        return speed;
+    }
+
+    public double getDistance()
+    {
+        return distance;
+    }
+
+    public void setSpeed(double speed)
+    {
+        this.speed = speed;
+    }
+
+    public void setDistance(double distance)
+    {
+        this.distance = distance;
+    }
+
+    public void setName(String name)
+    {
+
+        this.name = name;
+    }
+    public void move(){
+        distance += speed*Math.random();
+    }
+    public void print(){
+        int rounded =(int) Math.round(distance);
+        for (int i = 0; i <rounded ; i++)
+        {
+            System.out.print(".");
+        }
+        System.out.println(name);
+    }
 }
